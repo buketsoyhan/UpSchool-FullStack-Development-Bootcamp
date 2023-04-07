3b7c4831e23c3c9007f39ad59f54752a7e72231b
Buket Soyhan <buketsoyhan1998@outlook.com>
2023-04-08 00:35:27 +0300

    Added Get All Queries for Address.

---
+++ Ödev3/UpStorage/src/Application/Features/Addresses/Queries/GetAll/AddressGetAllQuery.cs
@@ -0,0 +1,13 @@
+﻿namespace Application.Features.Addresses.Queries.GetAll
+{
+    public class AddressGetAllQuery
+    {
+        public int CountryId { get; set; }
+        public int CityId { get; set; }
+        public AddressGetAllQuery(int countryId, int cityId)
+        {
+            CountryId = countryId;
+            CityId= cityId;
+        }
+    }
+}
---
+++ Ödev3/UpStorage/src/Application/Features/Addresses/Queries/GetAll/AddressGetAllQueryHandler.cs
@@ -0,0 +1,55 @@
+﻿using Application.Common.Interfaces;
+using Application.Features.Cities.Queries.GetAll;
+using Domain.Entities;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace Application.Features.Addresses.Queries.GetAll
+{
+    public class AddressGetAllQueryHandler:IRequestHandler<AddressGetAllQuery, List<AddressGetAllDto>>
+    {
+        private readonly IApplicationDbContext _applicationDbContext;
+
+        public AddressGetAllQueryHandler(IApplicationDbContext applicationDbContext)
+        {
+            _applicationDbContext = applicationDbContext;
+        }
+
+        public async Task<List<AddressGetAllDto>> Handle(AddressGetAllQuery request, CancellationToken cancellationToken)
+        {
+            var dbQuery = _applicationDbContext.Addresses.AsQueryable();
+
+            dbQuery = dbQuery.Where(x => x.CountryId == request.CountryId);
+
+            dbQuery = dbQuery.Include(x => x.Country);
+
+            var addresses = await dbQuery.ToListAsync(cancellationToken);
+
+            var addressDtos = MapAddressToGetAllDtos(addresses);
+
+            return addressDtos.ToList();
+        }
+
+        private IEnumerable<AddressGetAllDto> MapAddressToGetAllDtos(List<Address> addresses)
+        {
+            List<AddressGetAllDto> addressGetAllDtos = new List<AddressGetAllDto>();
+
+            foreach (var address in addresses)
+            {
+
+                yield return new AddressGetAllDto()
+                {
+                    Name = address.Name,
+                    UserId= address.UserId,
+                    CountryId= address.CountryId,
+                    CityId= address.CityId,
+                    District = address.District,
+                    PostCode= address.PostCode,
+                    AddressLine1 = address.AddressLine1,
+                    AddressLine2= address.AddressLine2,   
+                };
+            }
+        }
+
+    }
+}
