d823ce85e73826d115f8254430ab664235b1671f
Buket Soyhan <buketsoyhan1998@outlook.com>
2023-04-08 04:49:06 +0300

    Note Configurations

--- Ödev3/UpStorage/src/Infrastructure/Persistence/Configurations/Application/CategoryConfiguration.cs
+++ Ödev3/UpStorage/src/Infrastructure/Persistence/Configurations/Application/CategoryConfiguration.cs
@@ -1,6 +1,27 @@
-﻿namespace Infrastructure.Persistence.Configurations.Application
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Persistence.Configurations.Application
 {
-    public class CategoryConfiguration
+    public class CategoryConfiguration : IEntityTypeConfiguration<Category>
     {
+        public void Configure(EntityTypeBuilder<Category> builder)
+        {
+            //Id
+            builder.HasKey(x => x.Id);
+            builder.Property(x => x.Id).ValueGeneratedOnAdd();
+
+            //Name
+            builder.Property(x => x.Name).IsRequired();
+            builder.Property(x => x.Name).HasMaxLength(150);
+
+            //User Id
+            builder.Property(x => x.UserId).IsRequired();
+            builder.Property(x => x.UserId).HasMaxLength(150);
+
+            builder.ToTable("Categories");
+
+        }
     }
 }
--- Ödev3/UpStorage/src/Infrastructure/Persistence/Configurations/Application/NoteConfiguration.cs
+++ Ödev3/UpStorage/src/Infrastructure/Persistence/Configurations/Application/NoteConfiguration.cs
@@ -8,10 +8,28 @@ namespace Infrastructure.Persistence.Configurations.Application
     {
         public void Configure(EntityTypeBuilder<Note> builder)
         {
+            //Id
+            builder.HasKey(x => x.Id);
+            builder.Property(x => x.Id).ValueGeneratedOnAdd();
+
+            //Title
+            builder.Property(x => x.Title).IsRequired(false);
+            builder.Property(x => x.Title).HasMaxLength(150);
+
+            //Content
+            builder.Property(x => x.Content).IsRequired();
+            builder.Property(x => x.Content).HasMaxLength(150);
+
+            //User Id
+            builder.Property(x => x.UserId).IsRequired();
+            builder.Property(x => x.UserId).HasMaxLength(150);
+
+            builder.ToTable("Notes");
+
             //Relationships
-            builder.HasOne<Note>(x => x.Note)
-                .WithMany(x => x.NoteCategories)
-                .HasForeignKey();
+            //builder.HasOne<Note>(x => x.Note)
+            //    .WithMany(x => x.NoteCategories)
+            //    .HasForeignKey();
         }
     }
 }
