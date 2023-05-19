a5145bb7ab9c30edd44f1a4259c901a2b223f0e0
Buket Soyhan <buketsoyhan1998@outlook.com>
2023-05-19 23:50:33 +0300

    Email empty or null and correct user id tests

--- Ödev4/tests/UpSchool.Domain.Tests/Services/UserServiceTests.cs
+++ Ödev4/tests/UpSchool.Domain.Tests/Services/UserServiceTests.cs
@@ -30,5 +30,44 @@ namespace UpSchool.Domain.Tests.Services
 
             Assert.Equal(expectedUser, user);
         }
+
+        [Fact]
+        public async Task AddAsync_ShouldThrowException_WhenEmailIsEmptyOrNull()
+        {
+            //Arrange
+            var userRepositoryMock = A.Fake<IUserRepository>();
+            var userService = new UserManager(userRepositoryMock);
+            var cancellationSource = new CancellationTokenSource();
+
+            // Act and Assert
+            //Email Null Check
+            await Assert.ThrowsAsync<ArgumentException>(() => userService.AddAsync("Buk","Soyh",24,null, cancellationSource.Token));
+            //Email Empty Check
+            await Assert.ThrowsAsync<ArgumentException>(() => userService.AddAsync("B", "S", 25, string.Empty, cancellationSource.Token));
+        }
+
+        [Fact]
+        public async Task AddAsync_ShouldReturn_CorrectUserId()
+        {
+            //Arrange
+            var userRepositoryMock = A.Fake<IUserRepository>();
+            var userService = new UserManager(userRepositoryMock);
+            Guid userId = new Guid("8f319b0a-2428-4e9f-b7c6-ecf78acf00f9");
+            var cancellationSource = new CancellationTokenSource();
+
+            var expectedUser = new User()
+            {
+                Id = userId
+            };
+
+            A.CallTo(() => userRepositoryMock.GetByIdAsync(userId, cancellationSource.Token))
+            .Returns(Task.FromResult(expectedUser));
+
+            // Act
+            var user = await userService.GetByIdAsync(userId, cancellationSource.Token);
+
+            // Assert
+            Assert.Equal(expectedUser, user);
+        }
     }
 }
